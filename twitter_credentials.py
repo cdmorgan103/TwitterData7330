31e8558aa33cd9e9566655405fc46ebabafdb75a
Chris Morgan <cdmorgan@mail.smu.edu>
2018-08-09 21:53:51 -0500

    add demo files

---
+++ twitter_credentials.py
@@ -0,0 +1,12 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "2225305578-utWNR1Fgr1nHqNJXYsU8UKFhpyAdya3Tg4chLOZ"
+ACCESS_TOKEN_SECRET = "cPXG4OrkUPQQb1sQTDTF0dMbSH797UJ0ZGKfZFOphNSRQ"
+CONSUMER_KEY = "zppuSEuJvADzNZkuyLXFYHcfI"
+CONSUMER_SECRET = "MbKLdXcyO6VxlSI48RevRllbEcDL4q5pKmd7bAqTtIzaEoAFNT"
+
+
+
+
+
+
