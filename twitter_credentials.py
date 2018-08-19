2ce3191b89a2d6c388b86fb4f83b29b20a8dc8b6
Chris Morgan <35780451+cdmorgan103@users.noreply.github.com>
2018-08-19 14:04:13 -0500

    update twitter credentials
    took out credentials

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,9 +1,9 @@
 # YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
 # Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = "2225305578-utWNR1Fgr1nHqNJXYsU8UKFhpyAdya3Tg4chLOZ"
-ACCESS_TOKEN_SECRET = "cPXG4OrkUPQQb1sQTDTF0dMbSH797UJ0ZGKfZFOphNSRQ"
-CONSUMER_KEY = "zppuSEuJvADzNZkuyLXFYHcfI"
-CONSUMER_SECRET = "MbKLdXcyO6VxlSI48RevRllbEcDL4q5pKmd7bAqTtIzaEoAFNT"
+ACCESS_TOKEN = "access token"
+ACCESS_TOKEN_SECRET = "access secret"
+CONSUMER_KEY = "cs key"
+CONSUMER_SECRET = "cskey secret"
 
 
 
